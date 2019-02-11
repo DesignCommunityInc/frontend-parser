c9203cae55e7e8503c169f31b7d71ffd822a128e
DCDRins <40469297+DCDRins@users.noreply.github.com>
2019-02-11 13:44:36 +0300

    v1.0

--- main-process/webview/dom-controller.js
+++ main-process/webview/dom-controller.js
@@ -1,35 +1,23 @@
 const $ = require('jquery');
 const { ipcMain, BrowserWindow } = require('electron');
-let win
+let page = {};
+let win = null;
 
-let page = {}
 
-function initialize(){
-    // Array.prototype.forEach.call(BrowserWindow.getAllWindows(), (window) => {
-    //     if (window.getTitle() == 'rins-app') 
-    //         win = window
-    // })
-    ipcMain.on('drawRectangle', (event, pos) => {
-        
-        
-        win.webContents.send('', pos)
-    }) 
+ipcMain.once('setBrowserWindowMain', (event, sender) => { 
+    win = sender; 
+    // event.sender.send('log', sender);
+})
 
-    // $(window).on('mousemove', function(e){
-    //     if(e.buttons == 1) {
-    //         if(rectangleTool_selected)
-    //         window.getSelection().removeAllRanges()
-    //         let param = domSelector(e)
-    //         $('.tmpDiv').css({ 
-    //             'width' : param.width,
-    //             'left' : param.left,
-    //             'height' : param.height,
-    //             'top' : param.top
-    //         })
-    //         console.log(param)
-    //     }
-    // })
-    
-}
-
-initialize()
+ipcMain.on('drawRectangle', (event, pos) => {
+    win.webContents.send('', pos);
+}) 
+ipcMain.once('onHierarchyCreated', (event, sender) => {
+    Array.prototype.forEach.call(BrowserWindow.getAllWindows(), (window) => {
+        if (window.getTitle() === 'rins-app') { 
+            win = window || {};
+        }
+    })
+    // event.sender.send('log', win);
+    win.webContents.send('onHierarchyCreated-reply', sender);
+})
--- main-process/webview/webview.js
+++ main-process/webview/webview.js
@@ -1,28 +1,35 @@
 const $ = require('jquery');
 const { ipcMain, BrowserWindow } = require('electron');
-let win
-
-let page = {}
+let page = {};
+let win;
 
 function initialize(){
-    
-    page.title = null
-
+    page.title = null;
     ipcMain.on('mouse-x-y-pos-changed', (event, pos) => {
         Array.prototype.forEach.call(BrowserWindow.getAllWindows(), (window) => {
             if (window.getTitle() == 'rins-app') 
                 win = window
-        })
-        win.webContents.send('mouse-x-y-pos-changed-reply', pos)
-    }) 
+        });
+        win.webContents.send('mouse-x-y-pos-changed-reply', pos);
+    }); 
     ipcMain.on('set-page-title', (event, title) => {
         Array.prototype.forEach.call(BrowserWindow.getAllWindows(), (window) => {
             if (window.getTitle() == 'rins-app') 
-                win = window
-        })
-        page.title = title
-        win.webContents.send('set-page-title-reply', `${title}`)
-    })
+                win = window;
+        });
+        page.title = title;
+        win.webContents.send('set-page-title-reply', `${title}`);
+    });
+    ipcMain.on('getContextList', (event) => {
+        let arr = []
+        for(let i = 0; i < 3; i++) {
+            arr.push({
+                text: 'text',
+                id: i
+            });
+        }
+        event.returnedValue = arr;
+    });
 }
 
 
