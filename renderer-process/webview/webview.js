de5326ee198beef7c120ad9effaf243853e746b1
DCDRins <erinilya000071@gmail.com>
2019-03-22 03:06:36 +0300

    ui-context menu for changing css rules at user

--- renderer-process/webview/webview.js
+++ renderer-process/webview/webview.js
@@ -17,8 +17,11 @@ Array.prototype.forEach.call(pageNodes, (node) => {
     node.addEventListener('contextmenu', function(event) {
         let e = event || window.event;
         e.stopPropagation();
-        let context = CSSWindowConstructor(DOMController.getCSSRules(node));
-        showContextMenu({pageX: e.pageX, pageY: e.pageY}, context);
+        let views = [
+            PropertiesWindowConstructor(DOMController.getCSSRules(node)),
+            CSSWindowConstructor(DOMController.getCSSRules(node))
+        ];
+        showContextMenu({pageX: e.pageX, pageY: e.pageY}, views);
     });
 });
 document.addEventListener('mousedown', function(event){
@@ -49,49 +52,121 @@ function wheel(ctrl, deltaY, position){
         else sendMainAsync('scale-changed', false);
     sendMainAsync('mouse-pos-changed-message', position);
 }
+
+function SetContextTitle(container, tmp, current){
+    if(tmp !== current){
+        let childItem = document.createElement('h2');
+        childItem.innerHTML = current;
+        container.append(childItem);
+        tmp = current;
+    }
+    return tmp
+}
 function CSSWindowConstructor(contextArray){
     let tmpFileName = "";
     let cssMonitor = document.createElement('div');
-    cssMonitor.classList.add('cssView');
-        Array.prototype.forEach.call(contextArray, (context) => {
-            let cssFileName = context.parentStyleSheet.href.split('/').slice(-1).pop() + '<br/>';
-            if(tmpFileName !== cssFileName){
-                let childItem = document.createElement('p');
-                childItem.innerHTML = cssFileName;
-                cssMonitor.append(childItem);
-                tmpFileName = cssFileName;
-            }
-            let childItem = document.createElement('span');
-            childItem.innerHTML += context.cssStringifyText;
-            cssMonitor.append(childItem);
-        });
+    cssMonitor.className = 'view view-active';
+    Array.prototype.forEach.call(contextArray, (context) => {
+        let cssFileName = context.parentStyleSheet.href.split('/').slice(-1).pop() + '<br/>';
+        tmpFileName = SetContextTitle(cssMonitor, tmpFileName, cssFileName);
+        let childItem = document.createElement('span');
+        childItem.innerHTML += context.cssStringifyText;
+        cssMonitor.append(childItem);
+    });
     return cssMonitor;
 }
 function PropertiesWindowConstructor(contextArray){
-    let tmpClassName = "";
+    let matches = /\b(?:margin|padding|backgroundPosition)\b/;
+    let doNotShow = /(^[0-9]+$)|\b[(?:cssText|parentStyleSheet|)]\b/;
     let cssMonitor = document.createElement('div');
-    cssMonitor.classList.add('cssView');
-        Array.prototype.forEach.call(contextArray, (context) => {
-            let cssClassName = context.selectorText;
-            if(tmpClassName !== cssClassName){
-                let childItem = document.createElement('p');
-                childItem.innerHTML = cssFileName;
-                cssMonitor.append(childItem);
-                tmpClassName = cssClassName;
-            }
-            Array.prototype.forEach.call(context.style, property => {
-                let childItem = document.createElement('span');
-                childItem.innerHTML += property;
-                cssMonitor.append(childItem);
-            });
+    let classListContainer = document.createElement('div');
+
+    classListContainer.className = 'list classList';
+    cssMonitor.className = 'view';
+    Array.prototype.forEach.call(contextArray, (context, i) => {
+        let spanWrapper = document.createElement('div');
+            spanWrapper.className = (i === 0) ? 'span-wrapper span-wrapper-active' : 'span-wrapper';
+
+        let cssClassName = context.selectorText;
+        
+        // set ClassList
+        let classTab = document.createElement('h3');
+        // classTab.addEventListener('click', function(){
+        //     let views = document.querySelectorAll('ui-context .span-wrapper');
+        //     let classTabs = document.querySelectorAll('ui-context h3');
+        //     let i = 0;
+        //     Array.prototype.forEach.call(classTabs, (tab, j) => {
+        //         i = (classTab === tab) ? j : 0;
+        //     });
+        //     Array.prototype.forEach.call(views, (view, j) => {
+        //         if(i === j) { view.className = 'span-wrapper span-wrapper-active'; return; }
+        //         if(i < j) view.className = 'span-wrapper span-wrapper-scaled';
+        //         else view.className = 'span-wrapper span-wrapper-scaled-invert'
+        //     });
+        // });
+        classTab.innerHTML = cssClassName;
+        classListContainer.append(classTab);
+
+        // set Content
+        let values = Object.assign({}, context.style);
+        let keys = Object.keys(context.style).slice(0);
+        Array.prototype.forEach.call(keys.filter((key) => { return values[key] !== ""; }), (cssProperty, i) => {
+            if(cssProperty.match(doNotShow)) return;
+            let childItem = document.createElement('span');
+            childItem.innerHTML = `${cssProperty}: ${values[cssProperty]}`;
+            delete keys[i];
+            delete values[cssProperty];
+            // if(cssProperty.match(matches)){
+            //     childItem.classList.add('matchContainer');
+            //     Array.prototype.forEach.call(context.style.filter((value)=>{return value.match(matches);}), matchProperty => {
+            //         let child = document.createElement('span');
+            //         child.innerHTML = matchProperty;
+            //         childItem.append(child);
+            //     });
+            // }
+            cssMonitor.append(childItem);
         });
+        Array.prototype.forEach.call(keys, cssProperty => {
+            if(cssProperty.match(doNotShow)) return;
+            let childItem = document.createElement('span');
+            childItem.innerHTML = cssProperty;
+            cssMonitor.append(childItem);
+        });
+        classListContainer.querySelector('h3').classList.add('active');
+        cssMonitor.prepend(classListContainer);
+    });
     return cssMonitor;
 }
-function showContextMenu(e, context) {
+function showContextMenu(e, views) {
     contextClear();
     setUIContextPosition(e);
-    uiContext.append(context);
+    setUIContextToolsList();
+    Array.prototype.forEach.call(views, view => {
+        uiContext.append(view);
+    });
     document.body.append(uiContext);
+}
+function setUIContextToolsList(){
+    let list = document.createElement('div');
+    list.className = 'list toolsList';
+    for(let i = 0; i < 5; i++){
+        let tool = document.createElement('h3');
+        switch(i){
+            case 0: case 1: case 2: case 3: case 4: {
+                tool.innerHTML = i;
+                tool.addEventListener('click', function(){
+                    let views = document.querySelectorAll('ui-context .view');
+                    Array.prototype.forEach.call(views, (view, j) => {
+                        if(i === j) { view.className = 'view view-active'; return; }
+                        if(i < j) view.className = 'view view-scaled';
+                        else view.className = 'view view-scaled-invert'
+                    });
+                });
+            } break;
+        }
+        list.append(tool);
+    }
+    uiContext.append(list);
 }
 function setUIContextPosition(e){
     uiContext.style.left = `${e.pageX}px`;
